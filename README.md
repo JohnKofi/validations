8daf8108211a347080fadbaf26392111cf0ea9e5
Kwesi Banson <kwesi_banson@hotmail.com>
2016-03-24 08:28:15 +0000

    removed unused functions

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # validations
-A custom validation of phone numbers in Ghana format
+A custom validation of phone numbers in local format. This can be used by beginners who want to get going with validating phone numbers on forms.
+
+# Format
+This will validate numbers in both local and international formats
--- phone_exec.php
+++ phone_exec.php
@@ -1,34 +1,27 @@
 <?php
-  //make sure all request are POST - this can be changed if neccesary
-  // all resonses are in json format so a javascript file can pick them up for AJAX processes
+  /*
+  *this accepts POST requests - it can be changed if neccesary
+  *all resonses are in json format so a javascript file can pick them up for AJAX processes
+  */
   if($_POST){
 
     if ($_POST['phone'] == "") {
       echo json_encode(array("resonse" => "0", "message" => "Please enter phone number!"));
       exit;
     }
-    /*
-    if ($_POST['password'] == "") {
-      echo json_encode(array("resonse" => "0", "message" => "Please enter password!"));
-      exit;
-    }
-    */
 
     if (validate_phone( $_POST['phone']) == true ) {
       echo json_encode(array("resonse" => "1", "message" => "Hurray phone number is valid!"));
     } else {
       echo json_encode(array("resonse" => "1", "message" => "Ouch the phone number does not look like a valid one!"));
     }
-    /*
-    if (passwordStrenght( $_POST['password']) == true ) {
-      echo json_encode(array("resonse" => "1", "message" => "That's a strong password you have there!"));
-    } else {
-      echo json_encode(array("resonse" => "1", "message" => "MMMMM!!! That's kinda weak please try another one!"));
-    }
-    */
+
 
   }
-
+    /*
+    * This function matches strings using a regex pattern. It can be modified to accept only local
+    * numbers without the international code (+233)
+    */
     function validate_phone($str){
         if(preg_match("/^\+?(233|0)[235]\d{8}$/", $str)){
              return true;
@@ -38,16 +31,5 @@
         }
     }
 
-    /*
-    function passwordStrenght($str){
-      #"%\A(?=[-_a-zA-Z0-9]*?[A-Z])(?=[-_a-zA-Z0-9]*?[a-Z])(?=[-_a-zA-Z0-9]*?[0-9])\S{6,10}\z%"
-      if(preg_match("%\A(?=[-_a-zA-Z0-9]*?[A-Z])(?=[-_a-zA-Z0-9]*?[a-Z])(?=[-_a-zA-Z0-9]*?[0-9])\S{6,10}\z%", $str)){
-           return true;
-         }
-         else {
-           return false;
-         }
-    }
-    */
 
  ?>
